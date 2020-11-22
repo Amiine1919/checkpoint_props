a28725db28b244d0fb4ae864cd34ac54af549aab
Amine Fathallah <fathala.amine@gmail.com>
2020-11-22 12:53:58 +0100

    final edit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Profile from "./Profile/Profile";
+
+const user={
+  name:"John Doe",
+  bio:"I love coding !",
+  profession:"junior web developer",
+  src: "/John-Doe.jpg"
+};
+
+
+export default function App() {
+  return (
+    <div>
+      <Profile name={user.name} bio={user.bio} profession={user.profession} src={user.src} />
+    </div>
+  );
+}
+
